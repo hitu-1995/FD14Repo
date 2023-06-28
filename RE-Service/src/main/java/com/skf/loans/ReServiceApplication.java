99691949106b85d55ec74e2518b121b1d32f5b70
hitu-1995 <hiteshwankhede11@gmail.com>
2023-06-28 10:57:32 +0530

    RE_SERVICE_CREATED

---
+++ RE-Service/src/main/java/com/skf/loans/ReServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skf.loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReServiceApplication.class, args);
+	}
+
+}
